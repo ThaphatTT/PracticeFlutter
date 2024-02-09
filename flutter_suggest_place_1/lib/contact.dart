d34f9df9df957ebd1e1913b4d205d7233646d716
Thaphat Meechaitana <128665086+ThaphatTT@users.noreply.github.com>
2024-02-09 16:20:09 +0700

    create class and build widget contactMe

--- flutter_suggest_place_1/lib/contact.dart
+++ flutter_suggest_place_1/lib/contact.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+
+class ContactMe extends StatelessWidget{
+  const ContactMe({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return Column(
+      children: [
+
+      ],
+    );
+  } 
+}
